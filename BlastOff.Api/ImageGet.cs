729b426596993b29fd804482882482b9cb5aa651
porthos42 <philopolis42@gmail.com>
2022-10-05 08:47:53 -1000

    Revert to basic version

--- BlastOff.Api/ImageGet.cs
+++ BlastOff.Api/ImageGet.cs
@@ -5,29 +5,43 @@ using Microsoft.Azure.CosmosRepository;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
+using System.Net.Http;
 using System.Threading.Tasks;
+
+
+
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Net.Http.Json;
+
+
 
 namespace BlastOff.Api
 {
     public class ImageGet
     {
-        private readonly IRepository<Image> _imageRepository;
-
-        public ImageGet(IRepository<Image> imageRepository) => _imageRepository = imageRepository;
+        private static string GetRandomDate()
+        {
+            var random = new Random();
+            var startDate = new DateTime(1995, 06, 16);
+            var range = (DateTime.Today - startDate).Days;
+            return startDate.AddDays(random.Next(range)).ToString("yyyy-MM-dd");
+        }
 
         [FunctionName("ImageGet")]
-        public async Task<IActionResult> Run(
+        public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "image")] HttpRequest req,
             ILogger log)
         {
-            var hasDays = int.TryParse(req.Query["days"], out var days);
-            log.LogInformation($"Requested images from last {days} days.");
-
-            var imageResponse = await _imageRepository.GetAsync
-                (img => img.Date > DateTime.Now.AddDays(-days));
-
-            return new OkObjectResult(imageResponse);
+            log.LogInformation("Executing ImageOfDayGet.");
+            var apiKey = Environment.GetEnvironmentVariable("ApiKey");
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync($"https://api.nasa.gov/planetary/apod?api_key={apiKey}&hd=true&date={GetRandomDate()}");
+            var result = await response.Content.ReadAsStringAsync();
+            return new OkObjectResult(JsonConvert.DeserializeObject(result));
         }
     }
 }
--- BlastOff.Shared/Image.cs
+++ BlastOff.Shared/Image.cs
@@ -5,24 +5,15 @@ using System.ComponentModel;
 
 namespace BlastOff.Shared
 {
-    public class Image : Item
+    public class Image
     {
-        [JsonProperty("title")]
         public string Title { get; set; }
-
-        [JsonProperty("copyright")]
         public string Copyright { get; set; }
-
-        [JsonProperty("date")]
         public DateTime Date { get; set; }
-
-        [JsonProperty("explanation")]
         public string Explanation { get; set; }
-
-        [JsonProperty("url")]
         public string Url { get; set; }
-
-        public bool IsNew => Date > DateTime.Today.AddDays(-3);
-        public string PrettyDate => Date.ToString("MMMM dd, yyyy");
+        public string HdUrl { get; set; }
     }
+
+
 }
